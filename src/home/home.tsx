389369ae823f5f1f92a3164f056666beff701d84
Merey Zholdas <merey.90@gmail.com>
2022-10-04 02:58:27 +0200

    home style responsive (#27)

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@material-ui/core';
+import { Button, Grid, Typography } from '@material-ui/core';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -7,26 +7,38 @@ import './home.scss';
 
 export const Home: React.FC = () => (
   <div className="home-container">
-    <Typography className="home-name" variant="h3">
-      Mira Tulenova
-    </Typography>
-    <span className="home-links">
-      <a className="social-link" href="https://www.facebook.com/mira.tulenova" target="_blank">
-        <FacebookIcon fontSize="large" />
-      </a>
-      <a className="social-link" href="https://www.instagram.com/mira_tulenova/" target="_blank">
-        <InstagramIcon fontSize="large" />
-      </a>
-      <a className="social-link" href="https://www.instagram.com/mira_tulenova/" target="_blank">
-        <TwitterIcon fontSize="large" />
-      </a>
-      <Button component={Link} to="/concerts" variant="outlined">
-        MAIN SITE
-      </Button>
-      <Button component={Link} to="/contact" variant="outlined">
-        CONTACT
-      </Button>
-    </span>
+    <Grid container>
+      <Grid item xs={12} sm={6}>
+        <Typography className="home-name" variant="h3">
+          Mira Tulenova
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <span className="home-links">
+          <Grid container>
+            <Grid item xs={12}>
+              <a className="social-link" href="https://www.facebook.com/mira.tulenova" target="_blank">
+                <FacebookIcon fontSize="large" />
+              </a>
+              <a className="social-link" href="https://www.instagram.com/mira_tulenova/" target="_blank">
+                <InstagramIcon fontSize="large" />
+              </a>
+              <a className="social-link" href="https://www.instagram.com/mira_tulenova/" target="_blank">
+                <TwitterIcon fontSize="large" />
+              </a>
+            </Grid>
+            <Grid item xs={12}>
+              <Button component={Link} to="/concerts" variant="contained" color="secondary">
+                MAIN SITE
+              </Button>
+              <Button component={Link} to="/contact" variant="contained" color="secondary">
+                CONTACT
+              </Button>
+            </Grid>
+          </Grid>
+        </span>
+      </Grid>
+    </Grid>
   </div>
 );
 
